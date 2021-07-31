5375de1460c20552623385d95c0c715edff70d13
shihyi <brvast@gmail.com>
2021-07-31 18:21:08 +1200

    modify nav scroll function

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -3,44 +3,35 @@ $(function () {
   $('[data-toggle="popover"]').popover()
 })
 
-function main() {
-
-  (function () {
-     'use strict';
-
-     $(document).on('click', 'a.page-scroll', function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                scrollTop: target.offset().top - 40
-              }, 900);
-              return false;
-            }
-          }
-        });
-
-      // Show Menu on Book
-      $(window).bind('scroll', function() {
-          var navHeight = $(window).height() - 600;
-          if ($(window).scrollTop() > navHeight) {
-              $('.navbar-default').addClass('on');
-          } else {
-              $('.navbar-default').removeClass('on');
-          }
-      });
-
-    // Hide nav on click
-    $(document).on('click', '.navbar-nav li a', function (event) {
-      // check if window is small enough so dropdown is created
-      var toggle = $(".navbar-toggle").is(":visible");
-      if (toggle) {
-        $(".navbar-collapse").collapse('hide');
-      }
-    });
-
-  }());
-
+// scroll animate
+$(document).on('click', 'a.page-scroll', function() {
+  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+    if (target.length) {
+      $('html,body').animate({
+        scrollTop: target.offset().top - 40
+      }, 900);
+      return false;
+    }
   }
-  main();
+});
+
+// Show Menu on scroll
+$(window).bind('scroll', function() {
+  var navHeight = $(window).height() - 600;
+  if ($(window).scrollTop() > navHeight) {
+      $('.navbar-default').addClass('on');
+  } else {
+      $('.navbar-default').removeClass('on');
+  }
+});
+
+// Hide nav on click
+$(document).on('click', '.navbar-nav li a', function (event) {
+  // check if window is small enough so dropdown is created
+  var toggle = $(".navbar-toggle").is(":visible");
+  if (toggle) {
+    $(".navbar-collapse").collapse('hide');
+  }
+});
