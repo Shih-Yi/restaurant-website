7c820b9ea33a8c6a939958c617c70c53fc3a8d4b
shihyi <brvast@gmail.com>
2021-08-14 10:34:33 +1200

    refactoring menu page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,9 @@ class HomeController < ApplicationController
   def menu
     @dishes = Dish.all.order("position asc")
     @entree = @dishes.entree
+    @soup = @dishes.soup
+    @chicken = @dishes.chicken
+    @beef = @dishes.beef
+    @all_types = [@entree, @soup, @chicken, @beef]
   end
 end
