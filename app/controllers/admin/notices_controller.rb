6bcdb18a4963928bde9d1397739b3f01ca51cda7
Shih-Yi <brvast@gmail.com>
2022-06-11 10:00:04 +1200

    add admin::notice controller

---
+++ app/controllers/admin/notices_controller.rb
@@ -0,0 +1,26 @@
+class Admin::NoticesController < Admin::BaseController
+  before_action :set_notice, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @notice.update(notice_params)
+    @notice.save!
+    flash[:success] = "Successfully"
+    redirect_to admin_notice_path
+  end
+
+  private
+
+  def set_notice
+    @notice = Notice.first
+  end
+
+  def notice_params
+    params.require(:notice).permit(:title, :text, :start_at, :end_at, :published)
+  end
+end
